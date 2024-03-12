146526c49d902e003b44d309603155aedf46bec2
Plushtrap0101 <cardonaesteban0101@gmail.com>
2024-03-12 16:36:25 -0500

    HOJADEVIDA2.0

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
+//ANDO ESTRESADO PROFESOR
 
 
 function App() {
